d1e11d1a02c07c04067df702a989528a6c14c32b
Tim Cera <tim@cerazone.net>
2023-03-08 15:58:02 -0500

    [FIX] Found cmake options which should prevent finding system libs

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,11 +5,13 @@ set -ex # Abort on error.
 mkdir build
 cd build
 
-cmake -G "Unix Makefiles" \
+cmake -G "${CMAKE_GENERATOR}" \
       "${CMAKE_ARGS}" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH="${PREFIX}" \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DCMAKE_FIND_FRAMEWORK=NEVER \
+      -DCMAKE_FIND_APPBUNDLE=NEVER \
       "${SRC_DIR}"
 
 make
@@ -19,5 +21,5 @@ make install
 SKIP=""
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-  ctest -VV --output-on-failure -j${CPU_COUNT} ${SKIP}
+  ctest -VV --output-on-failure -j"${CPU_COUNT}" "${SKIP}"
 fi
